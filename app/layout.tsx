df636dd046fed571de73e2d4117001e2cc3d0ed4
Akie <legaspiernstdanielle@gmail.com>
2026-02-11 03:06:36 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 
 import './globals.css'
 
+import Image from 'next/image'
+
 export const metadata: Metadata = {
   title: 'Ernst legaspi - Portfolio',
   description: 'My Portfolio. You can see some of the projects that I developed so far, my skills, and experiences.'
@@ -9,8 +11,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <html lang='en'>
-    <body className='antialiased font-main'>
-      {children}
+    <body className='antialiased font-main bg-black/95 text-white flex flex-col items-center justify-center min-h-screen'>
+      <Image
+        alt='Penguin'
+        width={400}
+        height={400}
+        src='/penguin.webp'
+      />
+
+      <h1 className='text-[60px] mt-[-50px]'>🚧 Renovating 🚧</h1>
+      {/* {children} */}
     </body>
   </html>
 }
